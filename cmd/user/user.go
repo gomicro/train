d2cf1b65fc92f7d01b8214a1cebd6ba53cfede13
Daniel Hess <dan9186@gmail.com>
2021-05-04 00:30:16 -0700

    switch to use central client build

--- cmd/user/user.go
+++ cmd/user/user.go
@@ -1,25 +1,19 @@
 package user
 
 import (
-	"context"
-	"crypto/tls"
 	"fmt"
-	"net/http"
 	"os"
 
 	"github.com/gomicro/train/config"
 
-	"github.com/gomicro/trust"
 	"github.com/google/go-github/github"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"golang.org/x/oauth2"
 )
 
 var (
-	dryRun    bool
-	client    *github.Client
-	clientCtx context.Context
+	dryRun bool
+	client *github.Client
 )
 
 // UserCmd represents the root of the user command
@@ -27,40 +21,16 @@ var UserCmd = &cobra.Command{
 	Use:              "user",
 	Short:            "User specific release train commands",
 	Long:             `User specific release train commands`,
-	PersistentPreRun: configClient,
+	PersistentPreRun: setupCommand,
 }
 
-func configClient(cmd *cobra.Command, args []string) {
-	pool := trust.New()
-
-	certs, err := pool.CACerts()
+func setupCommand(cmd *cobra.Command, args []string) {
+	var err error
+	client, err = config.GetClient()
 	if err != nil {
-		fmt.Printf("failed to create cert pool: %v\n", err.Error())
+		fmt.Printf("Error: %v", err.Error())
 		os.Exit(1)
 	}
-
-	httpClient := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{RootCAs: certs},
-		},
-	}
-
-	config, err := config.ParseFromFile()
-	if err != nil {
-		fmt.Printf("error: %v", err.Error())
-		os.Exit(1)
-	}
-
-	clientCtx = context.Background()
-	clientCtx = context.WithValue(clientCtx, oauth2.HTTPClient, httpClient)
-
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{
-			AccessToken: config.Github.Token,
-		},
-	)
-
-	client = github.NewClient(oauth2.NewClient(clientCtx, ts))
 
 	dryRun = viper.GetBool("dryRun")
 }
