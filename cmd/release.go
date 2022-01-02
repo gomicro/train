a7471db3afd4052b05278a89e864804c81121b15
Daniel Hess <dan9186@gmail.com>
2022-01-01 18:08:07 -0800

    move commands to singular commands

---
+++ cmd/release.go
@@ -0,0 +1,70 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/gosuri/uiprogress"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(orgReleaseCmd)
+}
+
+var orgReleaseCmd = &cobra.Command{
+	Use:              "release [org_name|user_name]",
+	Short:            "Release PRs for an org or user's repos that can be merged",
+	PersistentPreRun: setupClient,
+	Run:              orgReleaseFunc,
+}
+
+func orgReleaseFunc(cmd *cobra.Command, args []string) {
+	ctx := context.Background()
+
+	uiprogress.Start()
+
+	if dryRun {
+		fmt.Println()
+		fmt.Println("===============")
+		fmt.Println("Doing a dry run")
+		fmt.Println("===============")
+	}
+
+	fmt.Println()
+
+	repos, err := clt.GetRepos(ctx, args[0])
+	if err != nil {
+		fmt.Printf("repos: %v\n", err.Error())
+		os.Exit(1)
+	}
+
+	if len(repos) < 1 {
+		fmt.Println("github: no repos found")
+		return
+	}
+
+	urls, err := clt.ReleaseRepos(ctx, repos, dryRun)
+	if err != nil {
+		fmt.Printf("releasing: %v\n", err.Error())
+		os.Exit(1)
+	}
+
+	uiprogress.Stop()
+
+	if len(urls) > 0 {
+		fmt.Println()
+		if dryRun {
+			fmt.Println("(Dryrun) Repos Released:")
+		} else {
+			fmt.Println("Repos Released:")
+		}
+
+		for _, url := range urls {
+			fmt.Println(url)
+		}
+
+		return
+	}
+}
--- cmd/root.go
+++ cmd/root.go
@@ -4,8 +4,16 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/gomicro/train/client"
+	"github.com/gomicro/train/config"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+)
+
+var (
+	base   string
+	clt    *client.Client
+	dryRun bool
 )
 
 func init() {
@@ -42,4 +50,25 @@ func Execute() {
 		fmt.Printf("Failed to execute: %v\n", err.Error())
 		os.Exit(1)
 	}
+}
+
+func setupClient(cmd *cobra.Command, args []string) {
+	c, err := config.ParseFromFile()
+	if err != nil {
+		fmt.Printf("Error: %v", err.Error())
+		os.Exit(1)
+	}
+
+	base = "release"
+	if c.ReleaseBranch != "" {
+		base = c.ReleaseBranch
+	}
+
+	clt, err = client.New(c.Github.Token, base)
+	if err != nil {
+		fmt.Printf("Error: %v", err.Error())
+		os.Exit(1)
+	}
+
+	dryRun = viper.GetBool("dryRun")
 }
