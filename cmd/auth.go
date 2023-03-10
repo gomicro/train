6c3e8ebd87c9790364e8bf0282ff8e87d8777454
Daniel Hess <dan9186@gmail.com>
2023-03-10 14:15:49 -0800

    some rework on auth func, don't feel like making it fully testable right now

--- cmd/auth.go
+++ cmd/auth.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"os"
@@ -17,105 +18,112 @@ import (
 	"golang.org/x/oauth2"
 )
 
-var (
-	reapprove    bool
-	clientID     string
-	clientSecret string
-)
-
 func init() {
-	rootCmd.AddCommand(authCmd)
-
-	authCmd.Flags().BoolVarP(&reapprove, "force", "f", false, "force train to reauth")
-}
-
-var authCmd = &cobra.Command{
-	Use:   "auth",
-	Short: "auth with github",
-	Long:  `authorize train against github`,
-	RunE:  authFunc,
+	rootCmd.AddCommand(NewAuthCmd(os.Stdout, openBrowser))
 }
 
 const (
 	state = "8be0d61c-eff3-4785-af45-da69eae4f226"
 )
 
-func authFunc(cmd *cobra.Command, args []string) error {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+var (
+	reapprove    bool
+	clientID     string
+	clientSecret string
+)
 
-	listener, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		cmd.SilenceUsage = true
-		return fmt.Errorf("auth: %w", err)
+func NewAuthCmd(out io.Writer, browserFunc func(string) error) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "auth",
+		Short: "auth with github",
+		Long:  `authorize train against github`,
+		RunE:  authRun(browserFunc),
 	}
 
-	port := listener.Addr().(*net.TCPAddr).Port
+	cmd.Flags().BoolVarP(&reapprove, "force", "f", false, "force train to reauth")
+	cmd.SetOut(out)
 
-	pool := trust.New()
+	return cmd
+}
 
-	certs, err := pool.CACerts()
-	if err != nil {
-		cmd.SilenceUsage = true
-		return fmt.Errorf("auth: %w", err)
+func authRun(browserFunc func(string) error) func(*cobra.Command, []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		listener, err := net.Listen("tcp", "127.0.0.1:0")
+		if err != nil {
+			cmd.SilenceUsage = true
+			return fmt.Errorf("auth: %w", err)
+		}
+
+		port := listener.Addr().(*net.TCPAddr).Port
+
+		pool := trust.New()
+
+		certs, err := pool.CACerts()
+		if err != nil {
+			cmd.SilenceUsage = true
+			return fmt.Errorf("auth: %w", err)
+		}
+
+		httpClient := &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{RootCAs: certs},
+			},
+		}
+
+		ctx = context.WithValue(ctx, oauth2.HTTPClient, httpClient)
+
+		conf := &oauth2.Config{
+			ClientID:     clientID,
+			ClientSecret: clientSecret,
+			Scopes:       []string{"repo"},
+			Endpoint: oauth2.Endpoint{
+				AuthURL:  "https://github.com/login/oauth/authorize",
+				TokenURL: "https://github.com/login/oauth/access_token",
+			},
+			RedirectURL: fmt.Sprintf("http://localhost:%v/auth", port),
+		}
+
+		token := make(chan string)
+
+		go startServer(ctx, listener, conf, token)
+
+		var opts []oauth2.AuthCodeOption
+		if reapprove {
+			opts = []oauth2.AuthCodeOption{oauth2.AccessTypeOffline, oauth2.ApprovalForce}
+		} else {
+			opts = []oauth2.AuthCodeOption{oauth2.AccessTypeOffline}
+		}
+
+		url := conf.AuthCodeURL(state, opts...)
+
+		err = browserFunc(url)
+		if err != nil {
+			cmd.SilenceUsage = true
+			return fmt.Errorf("auth: %w", err)
+		}
+
+		tkn := <-token
+		close(token)
+
+		c, err := config.ParseFromFile()
+		if err != nil {
+			cmd.SilenceUsage = true
+			return fmt.Errorf("auth: %w", err)
+		}
+
+		c.Github.Token = tkn
+
+		err = c.WriteFile()
+		if err != nil {
+			cmd.SilenceUsage = true
+			return fmt.Errorf("auth: %w", err)
+		}
+
+		return nil
 	}
-
-	httpClient := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{RootCAs: certs},
-		},
-	}
-
-	ctx = context.WithValue(ctx, oauth2.HTTPClient, httpClient)
-
-	conf := &oauth2.Config{
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		Scopes:       []string{"repo"},
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://github.com/login/oauth/authorize",
-			TokenURL: "https://github.com/login/oauth/access_token",
-		},
-		RedirectURL: fmt.Sprintf("http://localhost:%v/auth", port),
-	}
-
-	token := make(chan string)
-
-	go startServer(ctx, listener, conf, token)
-
-	var opts []oauth2.AuthCodeOption
-	if reapprove {
-		opts = []oauth2.AuthCodeOption{oauth2.AccessTypeOffline, oauth2.ApprovalForce}
-	} else {
-		opts = []oauth2.AuthCodeOption{oauth2.AccessTypeOffline}
-	}
-
-	url := conf.AuthCodeURL(state, opts...)
-
-	err = openBrowser(url)
-	if err != nil {
-		cmd.SilenceUsage = true
-		return fmt.Errorf("auth: %w", err)
-	}
-
-	tkn := <-token
-	close(token)
-
-	c, err := config.ParseFromFile()
-	if err != nil {
-		cmd.SilenceUsage = true
-		return fmt.Errorf("auth: %w", err)
-	}
-
-	c.Github.Token = tkn
-
-	err = c.WriteFile()
-	if err != nil {
-		cmd.SilenceUsage = true
-		return fmt.Errorf("auth: %w", err)
-	}
-
-	return nil
 }
 
 func startServer(ctx context.Context, listener net.Listener, conf *oauth2.Config, token chan string) {
@@ -168,16 +176,18 @@ func authHandler(ctx context.Context, conf *oauth2.Config, token chan string) fu
 }
 
 func openBrowser(url string) error {
+	ctx := context.Background()
+
 	switch runtime.GOOS {
 	case "linux":
-		err := exec.Command("xdg-open", url).Start()
-		return fmt.Errorf("open browser: %w", err)
+		err := exec.CommandContext(ctx, "xdg-open", url).Start()
+		return fmt.Errorf("open browser: linux: %w", err)
 	case "windows":
-		err := exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-		return fmt.Errorf("open browser: %w", err)
+		err := exec.CommandContext(ctx, "rundll32", "url.dll,FileProtocolHandler", url).Start()
+		return fmt.Errorf("open browser: windows: %w", err)
 	case "darwin":
-		err := exec.Command("open", url).Start()
-		return fmt.Errorf("open browser: %w", err)
+		err := exec.CommandContext(ctx, "open", url).Start()
+		return fmt.Errorf("open browser: darwin: %w", err)
 	default:
 		return fmt.Errorf("open browser: unsupported platform")
 	}
